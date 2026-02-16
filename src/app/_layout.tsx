9edadaaf9b461f099628a9d33ace9c0acd82af3d
SoulsTogetherX <xalvarez4444@gmail.com>
2026-02-16 12:46:12 -0800

    Organized folders and implemented expo-router

---
+++ src/app/_layout.tsx
@@ -0,0 +1,13 @@
+import { Slot } from "expo-router";
+import { View, Text } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+
+export default function RootLayout() {
+  return (
+    <SafeAreaProvider>
+      <Slot />
+      <StatusBar style="auto" />
+    </SafeAreaProvider>
+  );
+}
